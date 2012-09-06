b55d2097f7167c70258cce2a55420efc6f07de54
Arnaud Grausem <arnaud.grausem@gmail.com>
2012-09-06 16:37:34 +0200

    uri and source of specification as a new params

--- src/redreport/__init__.py
+++ src/redreport/__init__.py
@@ -10,18 +10,18 @@ config_file = os.path.join(os.path.dirname(__path__[0]), 'conf.yml')
 
 config = load(open(config_file, 'r'))
 teams = config['teams']
+general_conf = config['general']
+
 templates_env = Environment(loader=PackageLoader('redreport', 'templates'))
 templates_env.filters['dateformat'] = util.dateformat
 templates_env.filters['monthformat'] = util.monthformat
 
 try:
-    client = spyre.new_from_spec(
-        '/home/arnaud/Projets/reporting/chronos.json',
-        'https://chronos.u-strasbg.fr/redmine'
-    ) 
+    spec_func = getattr(spyre, 'new_from_%s' % general_conf['spec_source'])
+    client = spec_func(general_conf['spec_uri']) 
 except Exception as e:
     print(e)
 else:
     client.enable('auth.Header', header_name='X-Redmine-API-Key',
-        header_value=config['general']['api_key'])
+        header_value=general_conf['api_key'])
     client.enable('format.Json')
